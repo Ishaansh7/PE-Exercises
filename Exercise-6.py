27786da6b1158cc555ab34497da2836d3b48c854
Ishaan Sharma <talktoishaan@hotmail.com>
2023-06-20 18:43:18 +0530

    Edits are made here.

--- Exercise-6.py
+++ Exercise-6.py
@@ -1,7 +1,7 @@
 #POV:Imagine you are working on a social media platform that allows users to create and share content with their friends and followers. As part of the platform's messaging feature, you need to develop a functionality that enables users to reverse the order of their messages. This will add a fun twist to conversations and encourage creativity in expressing thoughts.
 print("Welcome to the Message Reverser!")
 n=(input("Please enter your message: "))
-print(n)
+
 print("Thank you for your message.")
 print("The reversed version of your message is: ")
-print(n[::-1])
+print(n[::-1]) #input string is reversed 
